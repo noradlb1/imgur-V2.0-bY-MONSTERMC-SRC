4e444a605c10bf7b5ee26baec4b9b1e3f9d34789
MONSTERMC <74623428+noradlb1@users.noreply.github.com>
2022-01-07 04:41:39 +0100

    imgur-V2.0-bY-MONSTERMC-SRC
    imgur-V2.0-bY-MONSTERMC-SRC

---
+++ imgurV2.0/Frm_Enviar.designer.vb
@@ -0,0 +1,260 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_Enviar
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Frm_Enviar))
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.txtFilePath = New System.Windows.Forms.TextBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.btnUpload = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.txtHtmlCode = New System.Windows.Forms.TextBox()
+        Me.btnCopy = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtBBCode = New System.Windows.Forms.TextBox()
+        Me.btnCopy2 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtDirectUrl = New System.Windows.Forms.TextBox()
+        Me.btnCopy3 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnBrowse.Location = New System.Drawing.Point(307, 42)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.btnBrowse.TabIndex = 1
+        Me.btnBrowse.Text = "..."
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'txtFilePath
+        '
+        Me.txtFilePath.AllowDrop = True
+        Me.txtFilePath.Location = New System.Drawing.Point(10, 44)
+        Me.txtFilePath.Name = "txtFilePath"
+        Me.txtFilePath.ReadOnly = True
+        Me.txtFilePath.Size = New System.Drawing.Size(291, 20)
+        Me.txtFilePath.TabIndex = 2
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(11, 215)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(371, 18)
+        Me.ProgressBar1.TabIndex = 42
+        '
+        'btnUpload
+        '
+        Me.btnUpload.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnUpload.Location = New System.Drawing.Point(11, 188)
+        Me.btnUpload.Name = "btnUpload"
+        Me.btnUpload.Size = New System.Drawing.Size(371, 23)
+        Me.btnUpload.TabIndex = 44
+        Me.btnUpload.Text = "Send"
+        Me.btnUpload.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.Filter = "ARQUIVOS(*.PNG;*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF;*.PNG;*.png|TODOS ARQUIVOS (*" &
+    ".*)|*.*"
+        Me.OpenFileDialog1.Tag = ""
+        Me.OpenFileDialog1.Title = "By HaInK SoOfTf"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 10
+        '
+        'txtHtmlCode
+        '
+        Me.txtHtmlCode.Location = New System.Drawing.Point(11, 83)
+        Me.txtHtmlCode.Name = "txtHtmlCode"
+        Me.txtHtmlCode.ReadOnly = True
+        Me.txtHtmlCode.Size = New System.Drawing.Size(290, 20)
+        Me.txtHtmlCode.TabIndex = 46
+        '
+        'btnCopy
+        '
+        Me.btnCopy.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnCopy.Location = New System.Drawing.Point(307, 81)
+        Me.btnCopy.Name = "btnCopy"
+        Me.btnCopy.Size = New System.Drawing.Size(75, 23)
+        Me.btnCopy.TabIndex = 45
+        Me.btnCopy.Text = "Copy"
+        Me.btnCopy.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(8, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 13)
+        Me.Label1.TabIndex = 47
+        Me.Label1.Text = "Caminho"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(8, 67)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(71, 13)
+        Me.Label2.TabIndex = 47
+        Me.Label2.Text = "HTML Code :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(8, 106)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 13)
+        Me.Label3.TabIndex = 50
+        Me.Label3.Text = "BBCode :"
+        '
+        'txtBBCode
+        '
+        Me.txtBBCode.Location = New System.Drawing.Point(11, 121)
+        Me.txtBBCode.Name = "txtBBCode"
+        Me.txtBBCode.ReadOnly = True
+        Me.txtBBCode.Size = New System.Drawing.Size(290, 20)
+        Me.txtBBCode.TabIndex = 49
+        '
+        'btnCopy2
+        '
+        Me.btnCopy2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnCopy2.Location = New System.Drawing.Point(307, 119)
+        Me.btnCopy2.Name = "btnCopy2"
+        Me.btnCopy2.Size = New System.Drawing.Size(75, 23)
+        Me.btnCopy2.TabIndex = 48
+        Me.btnCopy2.Text = "Copy"
+        Me.btnCopy2.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(8, 145)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(29, 13)
+        Me.Label4.TabIndex = 53
+        Me.Label4.Text = "URL"
+        '
+        'txtDirectUrl
+        '
+        Me.txtDirectUrl.Location = New System.Drawing.Point(11, 161)
+        Me.txtDirectUrl.Name = "txtDirectUrl"
+        Me.txtDirectUrl.ReadOnly = True
+        Me.txtDirectUrl.Size = New System.Drawing.Size(290, 20)
+        Me.txtDirectUrl.TabIndex = 52
+        '
+        'btnCopy3
+        '
+        Me.btnCopy3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnCopy3.Location = New System.Drawing.Point(307, 159)
+        Me.btnCopy3.Name = "btnCopy3"
+        Me.btnCopy3.Size = New System.Drawing.Size(75, 23)
+        Me.btnCopy3.TabIndex = 51
+        Me.btnCopy3.Text = "Copy"
+        Me.btnCopy3.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ProgressBar1)
+        Me.GroupBox1.Controls.Add(Me.txtDirectUrl)
+        Me.GroupBox1.Controls.Add(Me.btnCopy3)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtFilePath)
+        Me.GroupBox1.Controls.Add(Me.btnBrowse)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtBBCode)
+        Me.GroupBox1.Controls.Add(Me.btnCopy2)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.btnUpload)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtHtmlCode)
+        Me.GroupBox1.Controls.Add(Me.btnCopy)
+        Me.GroupBox1.Location = New System.Drawing.Point(7, 4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(388, 241)
+        Me.GroupBox1.TabIndex = 54
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Configurações"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(399, 11)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(260, 234)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 55
+        Me.PictureBox1.TabStop = False
+        '
+        'Frm_Enviar
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(664, 251)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Frm_Enviar"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Imgur V1.0 BY MONSTERMC"
+        Me.TopMost = True
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnBrowse As System.Windows.Forms.Button
+    Friend WithEvents txtFilePath As System.Windows.Forms.TextBox
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents btnUpload As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents txtHtmlCode As System.Windows.Forms.TextBox
+    Friend WithEvents btnCopy As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtBBCode As System.Windows.Forms.TextBox
+    Friend WithEvents btnCopy2 As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtDirectUrl As System.Windows.Forms.TextBox
+    Friend WithEvents btnCopy3 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+End Class
---
+++ imgurV2.0/Frm_Enviar.vb
@@ -0,0 +1,150 @@
+﻿Imports System.Text
+Imports System.Net
+Imports System.Text.RegularExpressions
+Imports System.IO
+Imports System.Security.Principal
+
+Public Class Frm_Enviar
+
+    Dim ClientId As String = "66665db7b4b0608"
+
+    Private Sub txtFilePath_DragDrop(ByVal sender As Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles txtFilePath.DragDrop
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            Dim MyFiles() As String
+            MyFiles = e.Data.GetData(DataFormats.FileDrop)
+            txtFilePath.Text = MyFiles(0)
+        End If
+    End Sub
+
+    Private Sub txtFilePath_DragEnter(ByVal sender As Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles txtFilePath.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.All
+        End If
+    End Sub
+
+    Public Function UploadImage(ByVal image As String)
+        Dim w As New WebClient()
+        w.Headers.Add("Authorization", "Client-ID " & ClientId)
+        Dim Keys As New System.Collections.Specialized.NameValueCollection
+        Try
+            Keys.Add("image", Convert.ToBase64String(File.ReadAllBytes(image)))
+            Dim responseArray As Byte() = w.UploadValues("https://api.imgur.com/3/image", Keys)
+            Dim result = Encoding.ASCII.GetString(responseArray)
+            Dim reg As New System.Text.RegularExpressions.Regex("link"":""(.*?)""")
+            Dim match As Match = reg.Match(result)
+            Dim url As String = match.ToString.Replace("link"":""", "").Replace("""", "").Replace("\/", "/")
+            Return url
+        Catch s As Exception
+            MessageBox.Show("Error!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Return "Failed!"
+        End Try
+    End Function
+
+    Private Sub btnBrowse_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBrowse.Click
+        Try
+            If OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+                txtFilePath.Text = OpenFileDialog1.FileName
+                PictureBox1.Image = Image.FromFile(OpenFileDialog1.FileName)
+            Else
+                txtFilePath.Text = Nothing
+                PictureBox1.Image = Nothing
+            End If
+        Catch ex As Exception
+            MsgBox(ex)
+        End Try
+    End Sub
+
+    Private Sub btnUpload_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnUpload.Click
+        trueup = False
+        Timer1.Start()
+    End Sub
+
+    Dim trueup As Boolean = False
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If ProgressBar1.Value >= 100 Then
+            Timer1.Stop()
+            ProgressBar1.SendToBack()
+            btnBrowse.Enabled = True
+            btnUpload.Enabled = True
+            btnCopy.Enabled = True
+            btnCopy2.Enabled = True
+            btnCopy3.Enabled = True
+            ProgressBar1.Value = 0
+        ElseIf ProgressBar1.Value = 50 Then
+            If trueup = False Then
+                trueup = True
+                Dim url As String = UploadImage(txtFilePath.Text)
+                If url.Contains("imgur") Then
+                    txtHtmlCode.Text = "<a href=""https://imgur.com/""><img src""" & url & """></img></a>"
+                    txtBBCode.Text = "[url=https://imgur.com/][img]" & url & "[/img][/url]"
+                    txtDirectUrl.Text = url
+                    MessageBox.Show("Imagem Enviada !", "Informações", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                End If
+            End If
+            ProgressBar1.Value += ++1
+        Else
+            btnBrowse.Enabled = False
+            btnUpload.Enabled = False
+            btnCopy.Enabled = False
+            btnCopy2.Enabled = False
+            btnCopy3.Enabled = False
+            ProgressBar1.BringToFront()
+            ProgressBar1.Value += ++1
+        End If
+    End Sub
+
+    Private Sub btnCopy_Click(sender As Object, e As EventArgs) Handles btnCopy.Click
+        Clipboard.SetText(txtHtmlCode.Text)
+        MessageBox.Show("URL Copiada", "Informações", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub btnCopy2_Click(sender As Object, e As EventArgs) Handles btnCopy2.Click
+        Clipboard.SetText(txtBBCode.Text)
+        MessageBox.Show("URL Copiada", "Informações", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub btnCopy3_Click(sender As Object, e As EventArgs) Handles btnCopy3.Click
+        Clipboard.SetText(txtDirectUrl.Text)
+        MessageBox.Show("URL Copiada", "Informações", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub txtHtmlCode_Click(sender As Object, e As EventArgs) Handles txtHtmlCode.Click
+        txtHtmlCode.SelectAll()
+    End Sub
+
+    Private Sub txtBBCode_Click(sender As Object, e As EventArgs) Handles txtBBCode.Click
+        txtBBCode.SelectAll()
+    End Sub
+
+    Private Sub txtDirectUrl_Click(sender As Object, e As EventArgs) Handles txtDirectUrl.Click
+        txtDirectUrl.SelectAll()
+    End Sub
+
+    Dim adel2, rehan As Integer
+    Dim newp As System.Drawing.Point
+
+    Private Sub PictureBox1_mousedown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs)
+        adel2 = MousePosition.X - Me.Location.X
+        rehan = MousePosition.Y - Me.Location.Y
+    End Sub
+
+    Private Sub PictureBox1_mousemove(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs)
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            newp = MousePosition
+            newp.X = newp.X - adel2
+            newp.Y = newp.Y - rehan
+            Me.Location = newp
+        End If
+    End Sub
+
+    Private Sub Frm_Enviar_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btnCopy.Enabled = False
+        btnCopy2.Enabled = False
+        btnCopy3.Enabled = False
+        btnBrowse.PerformClick()
+        If OpenFileDialog1.FileName = Nothing Then
+        Else
+            btnUpload.PerformClick()
+        End If
+    End Sub
+End Class
